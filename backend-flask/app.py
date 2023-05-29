3721820fc89e0fb537ae9e1f36a2410a2faff90f
phet2309 <phet2309@gmail.com>
2023-05-29 18:42:12 -0400

    Added notification feature

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -11,6 +11,7 @@ from services.search_activities import *
 from services.message_groups import *
 from services.messages import *
 from services.create_message import *
+from services.notifications_activities import *
 from services.show_activity import *
 
 app = Flask(__name__)
@@ -65,6 +66,11 @@ def data_home():
   data = HomeActivities.run()
   return data, 200
 
+@app.route("/api/activities/notifications", methods=['GET'])
+def data_notifications():
+  data = NotificationsActivities.run()
+  return data, 200
+
 @app.route("/api/activities/@<string:handle>", methods=['GET'])
 def data_handle(handle):
   model = UserActivities.run(handle)
@@ -98,7 +104,7 @@ def data_activities():
 
 @app.route("/api/activities/<string:activity_uuid>", methods=['GET'])
 def data_show_activity(activity_uuid):
-  data = ShowActivity.run(activity_uuid=activity_uuid)
+  data = ShowActivities.run(activity_uuid=activity_uuid)
   return data, 200
 
 @app.route("/api/activities/<string:activity_uuid>/reply", methods=['POST','OPTIONS'])
